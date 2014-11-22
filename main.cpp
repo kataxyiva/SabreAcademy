81ad2fe6a889f7e07e29ced0ea53e3eb72860922
Katarzyna K <kasper.kasia@gmail.com>
2014-11-22 10:00:05 +0100

    Stworzenie funkcji arytmetycznych

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
-
+#include <iostream>
+#include "multiple.h"
+#include "add.h"
 
 int main(){
-return 0;}
+std::cout << multiple(2,add(2,4)) << std::endl;
+	return 0;
+}
---
+++ multiple.h
@@ -0,0 +1,8 @@
+#ifndef MULTIPLE_H
+#define MULTIPLE_H
+
+int multiple(int a , int b){
+	return a * b;
+}
+
+#endif
